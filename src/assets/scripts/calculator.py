3a8946066c80d9cbae20098b3d499647a3109feb
crysthofferattier <crysthofferattier@gmail.com>
2023-10-20 19:37:31 +0100

    add calculator.py

--- src/assets/scripts/calculator.py
+++ src/assets/scripts/calculator.py
@@ -4,61 +4,17 @@ import sys
 import json
 
 FILE = sys.argv[1]
-# assets = None
 
 
 def read_file(file: str) -> json:
     return json.load(open(file))
 
 
-def calculate_total_per_assets(transactions: json, assets: json) -> None:
-
-    for asset in assets:
-        total = 0
-        total05 = 0
-        total06 = 0
-        total07 = 0
-        total08 = 0
-        total09 = 0
-        total10 = 0
-
-        for transaction in transactions:
-            if "-05-" in transaction['trade_date']:
-                if asset['symbol'] == transaction['asset']['symbol']:
-                    total05 += float(transaction['total'])
-            elif "-06-" in transaction['trade_date']:
-                if asset['symbol'] == transaction['asset']['symbol']:
-                    total06 += float(transaction['total'])
-            elif "-07-" in transaction['trade_date']:
-                if asset['symbol'] == transaction['asset']['symbol']:
-                    total07 += float(transaction['total'])
-            elif "-08-" in transaction['trade_date']:
-                if asset['symbol'] == transaction['asset']['symbol']:
-                    total08 += float(transaction['total'])
-            elif "-09-" in transaction['trade_date']:
-                if asset['symbol'] == transaction['asset']['symbol']:
-                    total09 += float(transaction['total'])
-            elif "-10-" in transaction['trade_date']:
-                if asset['symbol'] == transaction['asset']['symbol']:
-                    total10 += float(transaction['total'])
-
-            total += float(transaction['total'])
-
-        print(f"{asset['symbol']}/05: R$ {total05:.2f}")
-        print(f"{asset['symbol']}/06: R$ {total06:.2f}")
-        print(f"{asset['symbol']}/07: R$ {total07:.2f}")
-        print(f"{asset['symbol']}/08: R$ {total08:.2f}")
-        print(f"{asset['symbol']}/09: R$ {total09:.2f}")
-        print(f"{asset['symbol']}/10: R$ {total10:.2f}")
-        print()
-
-    print(f"TOTAL: R$ {total:.2f}")
-
-
-def calculate_total_per_month(transactions: json, assets: json) -> None:
-    months = ["-05-", "-06-", "-07-", "-08-", "-09-", "-10-"]
+def calculate_total_per_month(transactions: json) -> None:
+    months = ["-05-", "-06-", "-07-", "-08-", "-09-", "-10-"]    
 
     for month in months:
+        total = 0.00
         bbas3 = 0.00
         itsa4 = 0.00
         petr4 = 0.00
@@ -92,6 +48,10 @@ def calculate_total_per_month(transactions: json, assets: json) -> None:
                 case "XPLG11":
                     xplg11 += get_transaction_total(month, transaction)
 
+            
+            total += float(transaction['total'])
+    
+
         print(month)
         print(f"BBAS3:  R$ {bbas3:.2f}")
         print(f"ITSA4:  R$ {itsa4:.2f}")
@@ -101,7 +61,12 @@ def calculate_total_per_month(transactions: json, assets: json) -> None:
         print(f"CPTS11: R$ {cpts11:.2f}")
         print(f"HGLG11: R$ {hglg11:.2f}")
         print(f"KNCR11: R$ {kncr11:.2f}")
-        print(f"XPLG11: R$ {xplg11:.2f}")
+        print(f"XPLG11: R$ {xplg11:.2f}")        
+        print()
+
+    print(f"TOTAL: R$ {total:.2f}")
+        
+        
 
 
 def get_transaction_total(month, transaction) -> float:
@@ -113,10 +78,9 @@ def get_transaction_total(month, transaction) -> float:
 
 def main():
     assets = read_file("assets.json")
-    transactions1 = read_file(FILE)['data']
+    transactions = read_file(FILE)['data']
 
-    # calculate_total_per_assets(transactions, assets)
-    calculate_total_per_month(transactions1, assets)
+    calculate_total_per_month(transactions)
 
 
 if __name__ == "__main__":
